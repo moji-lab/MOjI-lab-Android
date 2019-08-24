60087567ad6f14e51882c24612df3fbaaca9758e
SongJemin <jemin56@naver.com>
2019-08-24 21:53:56 +0900

    Add RegisterActivity UI

---
+++ app/src/main/java/com/jem/moji/ui/register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.jem.moji.ui.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.jem.moji.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
