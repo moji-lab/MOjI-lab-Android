3d70ac31c38856e18080d3ca3359084c53b61e6a
SongJemin <jemin56@naver.com>
2019-08-24 18:23:29 +0900

    Add Splash Function

---
+++ app/src/main/java/com/jem/moji/ui/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.jem.moji.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jem.moji.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/jem/moji/ui/splash/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.jem.moji.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.jem.moji.R
+import com.jem.moji.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val hd = Handler()
+        hd.postDelayed(splashhandler(), 1500) // 3000ms=3초후에 핸들러 실행 //딜레이 3000
+    }
+
+    private inner class splashhandler : Runnable {
+        override fun run() {
+            var intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out) // fade in, fade out 애니메이션 효과
+            this@SplashActivity.finish() // 스플래쉬 페이지 액티비티 스택에서 제거
+        }
+    }
+}
