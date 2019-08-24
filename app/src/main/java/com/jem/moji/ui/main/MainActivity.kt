2fb5305c7114d91a5f1fd28e9043e78a1a05813c
SongJemin <jemin56@naver.com>
2019-08-24 16:12:47 +0900

    Init moji Setting

---
+++ app/src/main/java/com/jem/moji/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jem.moji.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jem.moji.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
