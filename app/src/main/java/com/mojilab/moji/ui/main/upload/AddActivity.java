81fbe1b36fbf5e40219d3866c85f0ffd9714f639
seunghee63 <0603yang@naver.com>
2019-09-18 05:36:04 +0900

    Connect upload acts & Add databinding,ViewModel on upload act

--- app/src/main/java/com/mojilab/moji/ui/main/upload/AddActivity.java
+++ app/src/main/java/com/mojilab/moji/ui/main/upload/AddActivity.java
@@ -1,7 +1,9 @@
 package com.mojilab.moji.ui.main.upload;
 
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.bumptech.glide.Glide;
 import com.mojilab.moji.R;
 
 public class AddActivity extends AppCompatActivity {
@@ -10,5 +12,8 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        //ImageView iv = findViewById(R.id.iv_add_act_upload_img);
+        //Glide.with(getApplication()).load("https://upload.wikimedia.org/wikipedia/commons/c/cb/IU_at_Golden_Disk_Awards_on_January_10%2C_2018_%281%29.jpg").into(iv);
     }
 }
---
+++ app/src/main/java/com/mojilab/moji/ui/main/upload/UploadNavigator.java
@@ -0,0 +1,13 @@
+package com.mojilab.moji.ui.main.upload;
+
+public interface UploadNavigator {
+
+    void callAddCourseActivity();
+
+    void callAddActivity();
+
+    void callChangeOrderActivity();
+
+    void callTagActivity();
+
+}
