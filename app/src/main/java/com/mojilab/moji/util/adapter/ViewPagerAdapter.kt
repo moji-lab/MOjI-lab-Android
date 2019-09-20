cc9f148500e7b2da9ade23f1b5bcc21a65373ac3
SongJemin <jemin56@naver.com>
2019-09-20 12:23:14 +0900

    Modify App Theme(dark->white)

--- app/src/main/java/com/mojilab/moji/util/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/mojilab/moji/util/adapter/ViewPagerAdapter.kt
@@ -8,12 +8,41 @@ import com.mojilab.moji.ui.main.mypage.myrecord.MyRecordFragment
 import com.mojilab.moji.ui.main.mypage.myscrab.MyScrabFragment
 
 class ViewPagerAdapter(fm : FragmentManager, private val num_fragment : Int) : FragmentStatePagerAdapter(fm) {
-    override fun getItem(position: Int): Fragment {
 
+    companion object{
+        private var myRecordFragment : MyRecordFragment? = null
+        private var friendShareFragment: FriendShareFragment? = null
+        private var myScrabFragment : MyScrabFragment? = null
+
+        @Synchronized
+        fun getMyRecordFragment() : MyRecordFragment{
+            if(myRecordFragment == null){
+                myRecordFragment = MyRecordFragment()
+            }
+            return myRecordFragment!!
+        }
+        @Synchronized
+        fun getFriendShareFragment() : FriendShareFragment{
+            if(friendShareFragment == null){
+                friendShareFragment = FriendShareFragment()
+            }
+            return friendShareFragment!!
+        }
+        @Synchronized
+        fun getMyScrabFragment() : MyScrabFragment{
+            if(myScrabFragment == null){
+                myScrabFragment = MyScrabFragment()
+            }
+            return myScrabFragment!!
+        }
+
+    }
+
+    override fun getItem(position: Int): Fragment {
         return when(position){
-            0 -> MyRecordFragment()
-            1 -> FriendShareFragment()
-            2 -> MyScrabFragment()
+            0 -> getMyRecordFragment()
+            1 -> getFriendShareFragment()
+            2 -> getMyScrabFragment()
             else -> null
         }!!
     }
