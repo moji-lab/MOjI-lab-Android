31ef66e9921aeef00898178462adaf61966b4ef1
KimMooHyeon <dlfb77@gmail.com>
2019-09-27 05:41:15 +0900

    finish detailFeed ui and making indicator like instargram

--- app/src/main/java/com/mojilab/moji/ui/main/feed/DetailFeed/DetailFeedDataPackage/DetailFeedRecyclerViewData.kt
+++ app/src/main/java/com/mojilab/moji/ui/main/feed/DetailFeed/DetailFeedDataPackage/DetailFeedRecyclerViewData.kt
@@ -3,7 +3,7 @@ package com.mojilab.moji.ui.main.feed.DetailFeed.DetailFeedDataPackage
 data class DetailFeedRecyclerViewData (
     var city : String?,
     var date: String?,
-    var RecyclerViewItem_Img: ArrayList<String?>,
+    var RecyclerViewItem_Img: ArrayList<FeedViewPagerData?>,
     var hashTag : String?,
     var heartNum : String?,
     var commentNum : String
--- app/src/main/java/com/mojilab/moji/util/adapter/DetailFeedRecyclerViewAdapter.kt
+++ app/src/main/java/com/mojilab/moji/util/adapter/DetailFeedRecyclerViewAdapter.kt
@@ -1,20 +1,12 @@
 package com.mojilab.moji.util.adapter
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
-import androidx.viewpager2.widget.ViewPager2
-import com.bumptech.glide.Glide
 import com.mojilab.moji.ui.main.feed.DetailFeed.DetailFeedDataPackage.DetailFeedRecyclerViewData
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayout
+import com.github.islamkhsh.CardSliderViewPager
 import com.mojilab.moji.R
 
 
@@ -31,19 +23,21 @@ class DetailFeedRecyclerViewAdapter(var ctx: Context, var dataList: ArrayList<De
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-
         holder.tv_item_detail_place.text=dataList[position].city
-        holder.vp_item_viewpager.adapter=SliderFeedPagerAdapter((ctx as AppCompatActivity).supportFragmentManager,dataList[position].RecyclerViewItem_Img.size,dataList[position].RecyclerViewItem_Img)
-        holder.vp_item_viewpager.offscreenPageLimit=2
-        holder.tl_item_detail_indicator.setupWithViewPager(holder.vp_item_viewpager)
-        //여기서 dataList[position].img 배열 viewpager로 넘기기
+        holder.vp_item_viewpager.adapter=SliderAdapter(ctx,dataList[position].RecyclerViewItem_Img)
+        holder.tv_item_detail_real_number.text=(position+1).toString()
     }
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        var tv_item_detail_real_number = itemView.findViewById(R.id.tv_item_detail_real_number) as TextView
         var tv_item_detail_place = itemView.findViewById(com.mojilab.moji.R.id.tv_item_detail_place) as TextView
-        var vp_item_viewpager = itemView.findViewById(com.mojilab.moji.R.id.vp_item_viewpager) as ViewPager
-        var tl_item_detail_indicator=itemView.findViewById(R.id.tl_item_detail_indicator) as TabLayout
+        var vp_item_viewpager = itemView.findViewById(com.mojilab.moji.R.id.vp_item_viewpager) as CardSliderViewPager
+
     }
 
+
+
+
 }
+
+
