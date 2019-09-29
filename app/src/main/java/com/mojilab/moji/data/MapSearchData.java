ff2f7308a92b6db07cc7207bfca1ad902764700d
seunghee63 <0603yang@naver.com>
2019-09-30 04:07:07 +0900

    Add bottom_sheet on map frag

---
+++ app/src/main/java/com/mojilab/moji/data/MapSearchData.java
@@ -0,0 +1,35 @@
+package com.mojilab.moji.data;
+
+public class MapSearchData{
+
+    public int id;
+    public String img;
+    public String mainAddress;
+    public String subAddress;
+    public float lat;
+    public float log;
+    public int likeCnt;
+    public boolean isLike;
+
+    public MapSearchData(){
+        this.id = id;
+        this.img = img;
+        this.mainAddress = mainAddress;
+        this.subAddress = subAddress;
+        this.lat = lat;
+        this.log = log;
+        this.likeCnt = likeCnt;
+        this.isLike = isLike;
+    }
+
+    public MapSearchData(int id, String img, String mainAddress, String subAddress, float lat, float log, int likeCnt, Boolean isLike){
+        this.id = id;
+        this.img = img;
+        this.mainAddress = mainAddress;
+        this.subAddress = subAddress;
+        this.lat = lat;
+        this.log = log;
+        this.likeCnt = likeCnt;
+        this.isLike = isLike;
+    }
+}
