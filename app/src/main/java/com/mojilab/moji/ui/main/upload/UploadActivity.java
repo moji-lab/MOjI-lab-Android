dd427598e7b24cad4eda6d0524b11605bc230dcd
seunghee63 <0603yang@naver.com>
2019-10-03 11:57:59 +0900

    add uri

--- app/src/main/java/com/mojilab/moji/ui/main/upload/UploadActivity.java
+++ app/src/main/java/com/mojilab/moji/ui/main/upload/UploadActivity.java
@@ -335,17 +335,12 @@ public class UploadActivity extends BaseActivity<ActivityUploadBinding, UploadVi
 
                 BitmapFactory.Options options = new BitmapFactory.Options();
 
-                InputStream input = new InputStream() {
-                    @Override
-                    public int read() throws IOException {
-                        return 0;
-                    }
-                };
+                InputStream input = null;
                 Log.e("test transform String :", courseDataItem.photos.get(j));
                 Log.e("test transform Uri :", Uri.parse(courseDataItem.photos.get(j)).toString());
 
-                Uri sArtworkUri = Uri.parse(courseDataItem.photos.get(j));
-                Uri uri = ContentUris.withAppendedId(sArtworkUri, 4);
+                Uri photosUri = Uri.parse(courseDataItem.photos.get(j)); //(Uri.parse("content://com.mojilab.moji/test_contents/"+ idStr));
+                Uri uri = ContentUris.withAppendedId(photosUri, 4);
 
                 try {
                     input = getContentResolver().openInputStream(uri);
